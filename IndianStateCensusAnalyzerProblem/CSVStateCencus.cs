2157d0e1df0af4d1fa7887005f7785192e24d42e
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-12-16 14:32:26 +0530

    [Lalit] Add: Indian State Census Analyzer

---
+++ IndianStateCensusAnalyzerProblem/CSVStateCencus.cs
@@ -0,0 +1,28 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyzerProblem
+{
+    public class CSVStateCencus
+    {
+        public int ReadStateCensusData(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csvReader.GetRecords<StateCencusDAO>().ToList();
+                foreach (var data in records)
+                {
+                    Console.WriteLine(data);
+                }
+                return records.Count() - 1;
+            }
+        }
+    }
+}
--- IndianStateCensusAnalyzerProblem/Program.cs
+++ IndianStateCensusAnalyzerProblem/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace IndianStateCensusAnalyzerProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyzer Problem Statement");
+        }
+    }
+}
